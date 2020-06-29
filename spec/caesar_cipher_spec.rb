005c5c3c3eaef1d8925514eaad868e11a2d5b1d1
Tilda Udufo <mathildaudufo@gmail.com>
2020-06-29 09:49:46 +0100

    Added rspec tests

---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,30 @@
+require './caesar_cipher'
+describe "#shift_letters" do
+  it "returns letters shifted by 1" do
+    expect(shift_letters('Aaa', 1)).to eql('Bbb')
+  end
+
+  it "returns letters shifted by 1" do
+    expect(shift_letters('Bbb', 1)).to eql('Ccc')
+  end
+
+  it "returns letters shifted back to the start(a/A)" do
+    expect(shift_letters('Zz', 1)).to eql('Aa')
+  end
+
+  it "returns shifted letters without shifting non-letters" do
+    expect(shift_letters('Love you Tilda!', 5)).to eql('Qtaj dtz Ynqif!')
+  end
+
+  it "shifts letters for large numbers" do
+    expect(shift_letters('Love you Tilda!', 75)).to eql('Ilsb vlr Qfiax!')
+  end
+
+  it "skips numbers when shifting" do
+    expect(shift_letters('My birhday is on the 17th!', 75)).to eql('Jv yfoeaxv fp lk qeb 17qe!')
+  end
+
+  it "preserves space when shifting" do
+    expect(shift_letters('My     birhday is on  the 17th!', 7)).to eql('Tf     ipyokhf pz vu  aol 17ao!')
+  end
+end
