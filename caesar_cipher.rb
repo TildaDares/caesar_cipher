6c2f9568053ab288b50ee53885aa3f689ebeca62
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-01 12:50:13 +0100

    Refactored

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,15 @@
 def shift_letters(string, number = 13)
-    shifted_letters = ""
+    shifted_letters = ''
     string.each_byte do |char|
         shift = char + number
         code = 0
         char < 97 ? code = 65 : code = 97
-        if(char >= 65 && char <= 90) || (char >= 97 && char <= 122) 
+        if (char >= 65 && char <= 90) || (char >= 97 && char <= 122)
             shifted_letters += ((((shift - code) % 26 + 26) % 26) + code).chr
         else shifted_letters += char.chr
         end
     end
-    return shifted_letters
+    shifted_letters
 end
 
 # puts "Enter a string and a number (separate with two spaces and the string must not contain digits)"
@@ -23,4 +23,4 @@ end
 #     end 
 #     string = chr_to_shift[0]
 #     number = chr_to_shift[1].to_i
-    puts shift_letters('My     birhday is on  the 17th!', 7)
+puts shift_letters('My     birhday is on  the 17th!', 7)
